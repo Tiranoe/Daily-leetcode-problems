232a9ce957007f53ab1041c9b8f0829360917a64
Tiranoe <jungwoo9543@gmail.com>
2022-10-21 16:48:13 -0400

    Palindrome problem solved with convert to string method

--- problem1.js
+++ problem1.js
@@ -1,10 +1,9 @@
 // Leetcode problem 9. Palindrome Number
-
 /* Given an integer x, return true if x is palindrome integer.
 An integer is a palindrome when it reads the same backward as forward.
 For example, 121 is a palindrome while 123 is not.
 */
-
+/*
 function Palindrome(x) {
     // return x === x.split('').reverse().join('');
     for(let i = 0; i < x.length / 2; i++){
@@ -15,3 +14,14 @@ function Palindrome(x) {
 }
 
 console.log(Palindrome(123))
+console.log(Palindrome(121))
+*/
+
+//If we want to use the convert the x into strings - I found this simple method
+
+function Palin(x) {
+    return (x.toString() == x.toString().split("").reverse().join(""))
+}
+
+console.log(Palin(123)) // false
+console.log(Palin(123454321)) // True
