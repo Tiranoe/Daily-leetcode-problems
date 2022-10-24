aa233fc7767d43b56db5d9ced5dbde43d78d0eb3
Tiranoe <jungwoo9543@gmail.com>
2022-10-24 15:51:56 -0400

    l1 and l2 were succesfully reversed

--- problem3.js
+++ problem3.js
@@ -4,3 +4,11 @@
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 */
 
+let addTwoNumbers = function(l1, l2) {
+    let number1 = l1.reverse().join("");
+    let number2 = l2.reverse().join("");
+    console.log(number1)
+    console.log(number2)
+};
+
+addTwoNumbers([2,4,3], [5,6,4]);
