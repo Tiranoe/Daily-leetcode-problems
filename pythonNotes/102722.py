d4b8d248affbc451c4e165328673faa5303621d7
Tiranoe <jungwoo9543@gmail.com>
2022-10-27 17:24:03 -0400

    understanding for loop python

---
+++ pythonNotes/102722.py
@@ -0,0 +1,12 @@
+## understanding each method of for loop
+
+nums = [1,2,3,4,5]
+
+#for i in range(len(nums)):
+#   print(i)
+
+#for num in nums:
+#    print(num)
+
+#for i, num in enumerate(nums):
+#    print(i, num)
