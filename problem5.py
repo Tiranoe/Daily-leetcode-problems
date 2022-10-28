803580c52e762fec5b621cb8dac75da8bf945d44
Tiranoe <jungwoo9543@gmail.com>
2022-10-27 20:05:04 -0400

    thought process added

--- problem5.py
+++ problem5.py
@@ -15,8 +15,6 @@
 
 # this answer does not work because str object has no attribute of 'pop' function
 def validAnagram(s,t):
-    s = 'anagram'
-    t = 'naagarm'
     if len(s) != len(t):
         return False
 
@@ -30,3 +28,15 @@ def validAnagram(s,t):
     # very useful!
 
 validAnagram('anagram', 'naagarm')
+
+#Putting my thought process i put on leetcode here
+    # we intially start with two string variables: s and t.
+    # Put these split function into an empty array
+    # We should need to split the string into each letters
+    # Iterate first letter of first string into the second string, and take them out if they match
+    # when the array becomes empty, we can confirm that t is an anagram of s.
+    # Output will be returned as true, otherwise, returned as false
+        
+        
+    ## Question to ask: Is there a simpler way to iterate this through sorting by alphabet letters in the array? Maybe a built in function for python?
+    #### thought of the fact that since anagram has to use up all letters to check, we can check for the length of the string and return false if it doesn't match up
