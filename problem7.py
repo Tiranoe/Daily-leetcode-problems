52ca2119edda1fd543ef81f73f5dc399a544c0e7
Tiranoe <jungwoo9543@gmail.com>
2022-10-31 18:05:53 -0400

    explained the problem, and pseudocoding how im going to solve this problem

---
+++ problem7.py
@@ -0,0 +1,14 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
+# and removing all non-alphanumeric characters, it reads the same forward and backward.
+# Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+## so to begin with, we understand that palindrome word has to be read same forward and backward.
+## note that any uppercase letters are converted into lower case letters
+## note that this could include numbers as well
+## we need to check if the word is palindrome and return output of true/false
+
+## How am i going to solve this solution?
+### I need to use 
