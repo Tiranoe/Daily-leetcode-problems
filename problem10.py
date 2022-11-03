28e734ff62c8a572dcd933ac69802ce30d06af87
Tiranoe <jungwoo9543@gmail.com>
2022-11-03 18:03:40 -0400

    problem solved

--- problem10.py
+++ problem10.py
@@ -15,3 +15,29 @@
 # Explanation: In this case, no transactions are done and the max profit = 0.
 
 
+## Pseudocode
+### In an array of prices where it iterates on indices and for each indices it results the prices of a given stock. (i stands for each day)
+### The point is to maximize profit by subtracting the price on 2nd day (of highest stock price) with the price on 1st day (the lowest day [hopefully?])
+### If there is a case of not gaining any profit at all, we return 0.
+### Also mindful that we need to buy FIRST then sell.
+
+### How are you going to solve the problem?
+#### I think since the given prices are an iterated array, we need to go through comparison and get the lowest number possible before subsituting.
+#### If there is no highest number than the first number in array, then we return 0
+#### After we get to find the lowest and highest integer in the array, we subtract those number to get the output. 
+
+def Solution(prices):
+    lowest = prices[0]
+    maxProfit = 0
+    for i in range(len(prices)):
+        if prices[i] < lowest:
+            lowest = prices[i]
+            # this gets the lowest number
+        elif prices[i] - lowest > maxProfit:
+            maxProfit = prices[i] - lowest
+            # this finds the biggest and maxProfit at the same time
+    return(maxProfit)
+
+Solution([7,1,5,3,6,4])
+
+# Solution works in leetcode
