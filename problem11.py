a7c88770ec6e5bc1fe4e76dc560d4da64a209319
Tiranoe <jungwoo9543@gmail.com>
2022-11-08 18:27:00 -0500

    included examples

--- problem11.py
+++ problem11.py
@@ -5,3 +5,8 @@
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
 # Every close bracket has a corresponding open bracket of the same type.
+
+## Examples
+# () => True
+# [](){} => True
+# (] => False
