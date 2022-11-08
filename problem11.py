d0963f353a039383afcecf94798d166a1ae99e47
Tiranoe <jungwoo9543@gmail.com>
2022-11-08 18:31:13 -0500

    add pseudocode, and started coding

--- problem11.py
+++ problem11.py
@@ -10,3 +10,12 @@
 # () => True
 # [](){} => True
 # (] => False
+
+## Pseudocode -
+### First thought is for the code to detect a opening parenthesis, bracket, block
+### Then we need to ensure that code checks for closing side right after the first one
+
+## I had the question of if it needs to close it right away - but problem description tells us that it needs to closed in correct order
+## Maybe we can use the for loop to go through the string s 
+
+def validParenthesis(s):
