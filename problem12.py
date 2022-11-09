4bc26f23a9c7354f58a782a8f00ed528ea9d1be9
Tiranoe <jungwoo9543@gmail.com>
2022-11-09 14:29:14 -0500

    problem solved

--- problem12.py
+++ problem12.py
@@ -18,6 +18,23 @@
 # when it find the location, we need to return the index of the target in the given array
 # if not, we return -1
 # NOTE that this needs to be algorithm with 0(log n) runtime complexity which means that we need to divide and conquer
+## this means that we need a left and right point and middle point to compare and throw away one side of the array since it's ascending order
 
-def binarySearch(nums):
-    
+def binarySearch(nums, target):
+    left = 0
+    right = len(nums - 1)
+
+    while left <= right:
+        middle = left + ((right - left) // 2)
+        if nums[middle] > target:
+            right = middle - 1
+        elif nums[middle] < target:
+            left = middle + 1
+        else:
+            return middle
+    return -1
+        
+# when finding the middle number = right+left/2 can lead to overflow(?) since it could be infinite.
+# correct way is middle = left + ((right - left) // 2)
+
+## solution accepted
