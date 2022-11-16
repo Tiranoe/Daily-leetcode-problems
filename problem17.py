38e60c2f02d796957706db691155aebcdcda9b96
Tiranoe <jungwoo9543@gmail.com>
2022-11-16 17:51:30 -0500

    first attempt to solve problem

--- problem17.py
+++ problem17.py
@@ -12,3 +12,17 @@
 # Input: root = []
 # Output: []
 
+## How am I going to solve this question?
+## First thing about the tree problem is that every time you go down a row:
+## The number of the numbers are multipled by 2 (1 -> 2 -> 4 -> 8, etc)
+## We can even find a mathematical way to check the root of binary tree with these numbers
+## maybe a for loop using the multipled by 2, method mentioned above?
+
+def invertTree(root):
+    for e in range(len(root)):
+        # create an empty array to put into every row?
+        
+        print(e)
+
+
+invertTree([4,2,7,1,3,6,9])
