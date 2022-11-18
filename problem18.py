f0e48a23c478f535ecf395f82c41ba78e6675d53
Tiranoe <jungwoo9543@gmail.com>
2022-11-18 16:13:15 -0500

    problem solved

--- problem18.py
+++ problem18.py
@@ -11,3 +11,22 @@
 # Input: root = [1,null,2]
 # Output: 2
 
+## How are you going to solve this code?
+# First, We need to think about using binary tree method to solve this solution
+# since we have the class of Tree (self, left, right)
+# first indicated if root is empty, then we return 0
+# second, we either decide to solve this recursive, iterative, or BFS(?)
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+def maxDepth(self, root) -> int:
+    if not root:
+        return 0
+
+    return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# Leetcode accepted problem
