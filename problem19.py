c57e1f010c913cf1aea38eeedc715f320a5060e7
Tiranoe <jungwoo9543@gmail.com>
2022-11-21 16:18:03 -0500

    resolving to understand hashmaps better

--- problem19.py
+++ problem19.py
@@ -1,1 +1,19 @@
-# Diamter of Binary Tree
+# I want to focus back to hashing and hashmap
+
+## Given an integer array nums, return true if any value 
+# appears at least twice in the array, 
+# and return false if every element is distinct.
+
+## I am going to solve this problem by using hashmap by introducting an empty object first
+
+def DuplicateNumbers(nums):
+    countNums = {}
+    for i in range(len(nums)):
+        countNums[nums[i]] = 1 + countNums(nums[i], 0)
+        if countNums[nums[i]] == 2:
+            return True
+
+nums = [1,2,3,1]
+DuplicateNumbers(nums)
+
+#solutions works in leetcode
