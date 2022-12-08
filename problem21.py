a64d9d8caf3a0295d5789b6baccb1ef58a7a4b72
Tiranoe <jungwoo9543@gmail.com>
2022-12-08 14:10:17 -0500

    understanding the problem

--- problem21.py
+++ problem21.py
@@ -11,3 +11,9 @@
 # Input: strs = ["dog","racecar","car"]
 # Output: ""
 # Explanation: There is no common prefix among the input strings.
+
+# Understading the problem
+# I believe we need to use the method of len(strs) to grab each length of each word in the array
+# And then use the If statement to grab the longest common prefix in the array
+# then return the longest common prefix or empty string if none exists
+
