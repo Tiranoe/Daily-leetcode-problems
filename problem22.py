684ee383b866dc442918bd4618c1811f74efd2a5
Tiranoe <jungwoo9543@gmail.com>
2022-12-10 18:28:03 -0500

    add custom judge

--- problem22.py
+++ problem22.py
@@ -11,3 +11,24 @@
 # Return k after placing the final result in the first k slots of nums.
 # Do not allocate extra space for another array. 
 # You must do this by modifying the input array in-place with O(1) extra memory.
+
+## Custom Judge
+## Custom Judge:
+
+'''
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.'''
+
