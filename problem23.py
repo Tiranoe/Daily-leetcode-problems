f73bffa106242d97b12b12c86354610d0f5cfc8e
Tiranoe <jungwoo9543@gmail.com>
2022-12-13 01:05:30 -0500

    examples added

--- problem23.py
+++ problem23.py
@@ -4,3 +4,15 @@
 # as runningSum[i] = sum(nums[0]…nums[i])
 # Return the running sum of nums.
 
+# Example
+## Input: nums = [1,2,3,4]
+## Output: [1,3,6,10]
+## Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+## Input: nums = [1,1,1,1,1]
+## Output: [1,2,3,4,5]
+## Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+## Input: nums = [3,1,2,10,1]
+## Output: [3,4,6,16,17]
+
