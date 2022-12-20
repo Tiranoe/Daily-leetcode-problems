6fe383f024a7e1cf10f7035bd1623c722a7ca9ea
Tiranoe <jungwoo9543@gmail.com>
2022-12-20 16:20:24 -0500

    trying to solve

--- problem27.py
+++ problem27.py
@@ -10,4 +10,17 @@
 
 # Input: head = [1,2,3,4,5,6]
 # Output: [4,5,6]
-# Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+# Explanation: Since the list has two middle nodes with values 3 and 4, 
+# we return the second one.
+
+## We can easily find the middle node of the linked list by dividing the total by 2
+## If it's a fraction, we will round it down (or take out the decimal)
+
+# can't do above method, since we are using linked list
+
+def middleNode(head):
+    fast = slow = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    return slow
