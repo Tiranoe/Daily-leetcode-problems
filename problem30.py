7b1ee5ac255e86d080c7fd0de905473c2eac4973
Tiranoe <jungwoo9543@gmail.com>
2022-12-26 23:47:03 -0500

    given examples added

--- problem30.py
+++ problem30.py
@@ -4,3 +4,13 @@
 # return the index if the target is found. 
 # If not, return the index where it would be if it were inserted in order.
 # You must write an algorithm with O(log n) runtime complexity.
+
+# Examples
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
