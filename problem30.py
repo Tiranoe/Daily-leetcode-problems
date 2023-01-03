9a1f310c47ead7c17ad303398689f485720a7a11
Tiranoe <jungwoo9543@gmail.com>
2023-01-03 15:01:06 -0500

    input how i was going to solve the problem

--- problem30.py
+++ problem30.py
@@ -14,3 +14,10 @@
 
 # Input: nums = [1,3,5,6], target = 7
 # Output: 4
+
+# step by step thoughts
+# so the first thing I thought of was to just iterate through the List nums to find the index
+# First example would bypass that with the method
+# second example would need to include if nums[i] is greater than target, you return i
+# third example would just return i, if it finish iterating but didn't not find anything
+
