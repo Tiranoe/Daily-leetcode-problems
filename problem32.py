ee470ef27bc1ea2cf85544006afe2ba297662a69
Tiranoe <jungwoo9543@gmail.com>
2023-01-05 18:10:47 -0500

    added examples

--- problem32.py
+++ problem32.py
@@ -11,3 +11,15 @@
 # M             1000
 # *note that 4, and 9 are IV, and IX and so on.
 
+# Example
+# Input: s = "III"
+# Output: 3
+# Explanation: III = 3.
+
+# Input: s = "LVIII"
+# Output: 58
+# Explanation: L = 50, V= 5, III = 3.
+
+# Input: s = "MCMXCIV"
+# Output: 1994
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
