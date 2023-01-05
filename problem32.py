84f4a3ac30b7ea9f98fd8f2b0b693a88eb12afb5
Tiranoe <jungwoo9543@gmail.com>
2023-01-05 18:10:38 -0500

    problem description added

--- problem32.py
+++ problem32.py
@@ -1,1 +1,13 @@
 #13. Roman to Integer
+
+# Given a roman numeral, convert it to an integer.
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# *note that 4, and 9 are IV, and IX and so on.
+
