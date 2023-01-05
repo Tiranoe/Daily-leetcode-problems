195c540c6ee744b89128b8fa9ca83faf75a3765f
Tiranoe <jungwoo9543@gmail.com>
2023-01-05 18:14:56 -0500

    code works!

--- problem32.py
+++ problem32.py
@@ -23,3 +23,22 @@
 # Input: s = "MCMXCIV"
 # Output: 1994
 # Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+def romanToInteger(s):
+    rti = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    s = s.replace('IV','IIII').replace('IX', 'VIIII').replace('XL','XXXX').replace('XC', 'LXXXX').replace('CD','CCCC').replace('CM','DCCCC')
+    sum = 0
+    for char in s:
+        sum += rti.get(char)
+    print(sum)
+
+s = "III"
+romanToInteger(s)
