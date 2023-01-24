0b2184482c0dc738bb61ddc1544319e19d85ba0e
Tiranoe <jungwoo9543@gmail.com>
2023-01-24 17:20:31 -0500

    add first example

--- problem34.py
+++ problem34.py
@@ -5,3 +5,8 @@
 # The returned integer should be non-negative as well.
 # You must not use any built-in exponent function or operator.
 # For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+# Example
+# Input: x = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
